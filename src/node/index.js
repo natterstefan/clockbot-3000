f47a7a9ccb612f19ae42345473525d16e137bc5f
Jan Amann <jan@amann.me>
2019-06-14 17:14:11 +0200

    wat

--- src/node/index.js
+++ src/node/index.js
@@ -2,8 +2,9 @@ const client = require('./client');
 const config = require('./config');
 const os = require('os');
 
+const message = process.argv.slice(2).join(' ') || 'Hello world';
+
 client.on('connect', () => {
-  const message = 'Hello mqtt'
   client.publish(config.TOPIC, os.hostname() + ': ' + message);
   client.end();
 });
